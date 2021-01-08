a1136f35c25a78b24480a816369771f3f6a6db95
Charlie Fischer <charlie.a.fischer@gmail.com>
2021-01-08 09:55:57 +0000

    updated from day 3

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,33 @@
+require_relative 'bike'
+
 class DockingStation
-  def release_bike
-    return Bike.new
+  attr_reader :bikes
+  attr_accessor :capacity
+
+  DEFAULT_CAPACITY = 20
+
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @bikes = []
+    @capacity = capacity
   end
 
-  def dock_bike
+  def release_bike
+    fail "No bikes available" if empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail "Limit reached" if full?
+    @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length >= @capacity
+  end
+
+  def empty?
+    @bikes == []
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,67 @@
-require "docking_station.rb"
-require 'bike.rb'
+require 'docking_station'
 
 describe DockingStation do
-
-  describe ".release_bike" do
-    it { is_expected.to respond_to(:release_bike) }
+  it "checking release_bike method" do
+    expect(subject).to respond_to :release_bike
   end
-  context "bike working?" do
-    it "bike working" do
-      expect(DockingStation.new.release_bike.working?).to eq("working")
+
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq [bike]
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bikes).to eq [bike]
+  end
+
+  it "releases a bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bikes).to eq [bike]
+  end
+
+  it 'responds to bike' do
+    expect(subject).to respond_to :bikes
+  end
+
+  describe '#release_bike' do
+    it "raises an error when no bike in dock" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
     end
   end
-  describe ".dock_bike" do
-    it { is_expected.to respond_to(:dock_bike) } 
+
+  it "raises an error when limit is reached" do
+    bike = Bike.new
+    station = DockingStation.new
+    DockingStation::DEFAULT_CAPACITY.times {station.dock Bike.new }
+    expect {station.dock(bike)}.to raise_error "Limit reached"
   end
+
+  it "set a @capacity instance variable" do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  it "test that the default capacity is 20" do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  describe 'initialization' do
+    it 'has a variable capacity' do
+      station = DockingStation.new(50)
+      bike = Bike.new
+      50.times {station.dock(bike)}
+      expect {station.dock(bike)}.to raise_error "Limit reached"
+    end
+  end
+
+
+
+
 end
